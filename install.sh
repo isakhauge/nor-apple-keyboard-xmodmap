71cb52d6d413cb38edaa57a4b37574887ec66cdd
Isak Hauge <isakhauge@gmail.com>
2020-11-28 09:48:38 +0100

    Discard copying of files. Simplify deployment.

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-XM_EXT=.Xmodmap
-XMOD_DIR=$HOME/$XM_EXT
-KEY_FILE=./keys$XM_EXT
-MOD_FILE=./mods$XM_EXT
-mkdir $XMOD_DIR
-cp $KEY_FILE $XMOD_DIR/$KEY_FILE
-cp $MOD_FILE $XMOD_DIR/$MOD_FILE
-echo "alias mkm=\"xmodmap ${MOD_FILE} && xmodmap ${KEY_FILE}\"" >> $HOME/.zshrc
+TITLE="# XMODMAP: Norwegian Apple Keyboard"
+DESC="# (MKL) Modify Keyboard Layout"
+BIN=xmodmap
+EXT=.Xmodmap
+KEYS=keys$EXT
+MODS=mods$EXT
+MSG="🍎️ Keyboard modified!"
+echo "" >> $HOME/.zshrc
+echo $TITLE >> $HOME/.zshrc
+echo $DESC >> $HOME/.zshrc
+echo "alias mkl=\"$BIN $PWD/$MODS && $BIN $PWD/$KEYS && echo $MSG\"" >> $HOME/.zshrc
