767974c8652df6cf63d654b2cc84ba98ad9e4734
Isak Hauge <isakhauge@gmail.com>
2020-11-28 08:24:42 +0100

    Create files

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+XM_EXT=.Xmodmap
+XMOD_DIR=$HOME/$XM_EXT
+KEY_FILE=./keys$XM_EXT
+MOD_FILE=./mods$XM_EXT
+mkdir $XMOD_DIR
+cp $KEY_FILE $XMOD_DIR/$KEY_FILE
+cp $MOD_FILE $XMOD_DIR/$MOD_FILE
+echo "alias mkm=\"xmodmap ${MOD_FILE} && xmodmap ${KEY_FILE}\"" >> $HOME/.bashrc
