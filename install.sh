77b6449443107bd85a5a5fddc3efdfb9f416e254
Isak Hauge <isakhauge@gmail.com>
2020-11-28 08:30:09 +0100

    Make small adjustments for Elementary OS

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ MOD_FILE=./mods$XM_EXT
 mkdir $XMOD_DIR
 cp $KEY_FILE $XMOD_DIR/$KEY_FILE
 cp $MOD_FILE $XMOD_DIR/$MOD_FILE
-echo "alias mkm=\"xmodmap ${MOD_FILE} && xmodmap ${KEY_FILE}\"" >> $HOME/.bashrc
+echo "alias mkm=\"xmodmap ${MOD_FILE} && xmodmap ${KEY_FILE}\"" >> $HOME/.zshrc
